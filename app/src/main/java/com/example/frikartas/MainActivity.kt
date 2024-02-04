2eb6ba50794503fce6dfa8023543f1b058be6cf1
Eric_Sans <esansvi@gmail.com>
2024-02-04 02:55:02 +0100

    inicio del módulo de DI, creación de modal drawer, scaffold con top y bottom app bar basicas (se irá implementando funcionalidades poco a poco)

--- app/src/main/java/com/example/frikartas/MainActivity.kt
+++ app/src/main/java/com/example/frikartas/MainActivity.kt
@@ -1,13 +1,49 @@
 package com.example.frikartas
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,19 +53,45 @@ import com.example.frikartas.ui.screens.CardDetailScreen
 import com.example.frikartas.ui.screens.CardListScreen
 import com.example.frikartas.ui.theme.FrikartasTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FrikartasTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                val scope = rememberCoroutineScope()
+
+                ModalNavigationDrawer(
+                    drawerContent = {
+                        Column(Modifier.fillMaxWidth()){
+                            DrawerContent(drawerItems, drawerState, scope)
+                        }
+                    },
+                    drawerState = drawerState
                 ) {
-                    FrikartasNavigation()
+                    Scaffold(
+                        topBar = { AppTopBar("Frikartas", {
+                            scope.launch {
+                                if (drawerState.isClosed) {
+                                    drawerState.open()
+                                } else {
+                                    drawerState.close()
+                                }
+                            }
+                        }) },
+                        bottomBar = { AppBottomBar() }
+                    ) { innerPadding ->
+                        Surface(
+                            modifier = Modifier.padding(innerPadding)
+                        ) {
+                            FrikartasNavigation()
+                        }
+                    }
                 }
             }
         }
@@ -55,5 +117,85 @@ fun FrikartasNavigation() {
             val collectionName = backStackEntry.arguments?.getString("collectionName") ?: ""
             CollectionDetailScreen(navController, collectionName)
         }
+    }
+}
+
+data class DrawerItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+) {
+    val badgeCount: Any = 0
+}
+
+val drawerItems = listOf(
+    DrawerItem(
+        title = "All",
+        selectedIcon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home,
+    ),
+    DrawerItem(
+        title = "Settings",
+        selectedIcon = Icons.Filled.Settings,
+        unselectedIcon = Icons.Outlined.Settings,
+    ),
+    DrawerItem(
+        title = "Información",
+        selectedIcon = Icons.Filled.Info,
+        unselectedIcon = Icons.Outlined.Info,
+    ),
+
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DrawerContent(drawerItems: List<DrawerItem>, drawerState: DrawerState, scope: CoroutineScope) {
+    var selectedItemIndex by rememberSaveable { mutableStateOf(0) }
+    val contextForToast = LocalContext.current
+
+    ModalDrawerSheet {
+        drawerItems.forEachIndexed { index, item ->
+            NavigationDrawerItem(
+                label = { Text(item.title) },
+                selected = index == selectedItemIndex,
+                onClick = {
+                    selectedItemIndex = index
+                    scope.launch { drawerState.close() }
+                    Toast.makeText(contextForToast, "${item.title} clicked", Toast.LENGTH_SHORT).show()
+                },
+                icon = {
+                    Icon(
+                        imageVector = if (index == selectedItemIndex) item.selectedIcon else item.unselectedIcon,
+                        contentDescription = item.title
+                    )
+                },
+                // Lógica para la insignia
+            )
+        }
+
+        Image(
+            painter = painterResource(id =R.drawable.frikartaslogo),
+            contentDescription = "Dragon Ball",
+            Modifier.size(500.dp))
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBar(title: String, onNavigationIconClick: () -> Unit) {
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(onClick = onNavigationIconClick) {
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu")
+            }
+        }
+    )
+}
+
+@Composable
+fun AppBottomBar() {
+    BottomAppBar {
+        // Agrega los botones o acciones que necesitas aquí
     }
 }
